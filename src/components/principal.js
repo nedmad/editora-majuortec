6009f6376e931ee4a56f630f2c188cd136cb47a1
nedmad <matheusmned@gmail.com>
2024-04-17 00:27:59 -0300

    first-

---
+++ src/components/principal.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Home from "./home";
+const Principal = () =>{
+    return (
+        <main className="principal">
+            <Home></Home>
+        </main>
+    )
+}
+
+export default Principal;
